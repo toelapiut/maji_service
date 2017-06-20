7b0244d1fe1c75eca0943c9c1f778ddbac3f430e
toelapiut <toelapiut7@gmail.com>
2017-06-20 12:37:44 +0300

    adding the logic interface

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ Price.prototype.cost = function() {
 
   return this.litre + this.time;
 };
+
+$(document).ready(function() {
+  $("form#location").submit(function(event) {
+    event.preventDefault();
+
+    var litre = parseInt($("#litre").val());
+    var time = parseInt($("#time").val());
+    var newPrice = new Price(litre, time);
+    $("#show-price").show();
+    $("#price").text(newPrice.cost());
+  });
+
+});
