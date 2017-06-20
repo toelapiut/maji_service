8ae80442a9b726e1335963f99143798fd1387094
Eric Wanjohi <ericwanjohi.ew@gmail.com>
2017-06-20 16:34:57 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+// Testimonials by Eric...
+$(document).ready(function(){
+   flag=1;
+    $("#nex").click(function(){
+       if(flag==0)
+        {
+            $("#side1").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side3").css("z-index","9");
+            $("#side1").css("transform","translateX(0px) scale(1.5)");
+            $("#side2").css("transform","translateX(-100px) scale(1)");
+            $("#side3").css("transform","translateX(100px)");
+            flag=1;
+        }
+        else if(flag==1)
+        {
+            $("#side3").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side3").css("transform","translateX(0px) scale(1.5)");
+            $("#side1").css("transform","translateX(-100px) scale(1)");
+            $("#side2").css("transform","translateX(100px)");
+            flag=2;
+        }
+        else if(flag==2)
+        {
+            $("#side2").css("z-index","999");
+            $("#side3").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side2").css("transform","translateX(0px) scale(1.5)");
+            $("#side3").css("transform","translateX(-100px) scale(1)");
+            $("#side1").css("transform","translateX(100px)");
+            flag=0;
+        }
+    });
+    $("#pre").click(function(){
+       if(flag==0)
+        {
+            $("#side3").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side3").css("transform","translateX(0px) scale(1.5)");
+            $("#side1").css("transform","translateX(-100px) scale(1)");
+            $("#side2").css("transform","translateX(100px)");
+            flag=2;
+        }
+        else if(flag==1)
+        {
+            $("#side2").css("z-index","999");
+            $("#side3").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side2").css("transform","translateX(0px) scale(1.5)");
+            $("#side3").css("transform","translateX(-100px) scale(1)");
+            $("#side1").css("transform","translateX(100px)");
+            flag=0;
+        }
+        else if(flag==2)
+        {
+            $("#side1").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side3").css("z-index","9");
+            $("#side1").css("transform","translateX(0px) scale(1.5)");
+            $("#side2").css("transform","translateX(-100px) scale(1)");
+            $("#side3").css("transform","translateX(100px)");
+            flag=1;
+        }
+    });
+});
+/*Please dont edit and if you do comment on the line...*/
